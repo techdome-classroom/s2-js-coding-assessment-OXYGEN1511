94b3507b611dd7b4f84706d43564e4a377d4b563
tusharay1511 <tusharay1511@gmail.com>
2024-09-27 14:11:06 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,26 +3,6 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // const Mystack = [];
-    // const pairOF = {
-    //     ')': '(',
-    //     '}': '{',
-    //     ']': '['
-    // };
-
-    // for (let ch of s) {
-        
-    //     if (ch in pairOF) {
-          
-    //         if (Mystack.pop() !== pairOF[ch]) {
-    //             return false;
-    //         }
-    //     }
-    //     else if (ch === '(' || ch === '{' || ch === '[') {
-    //         Mystack.push(ch);
-    //     }
-    // }
-    // return Mystack.length === 0;
     const stack = [];
 
     for (let char of s) {
