53a9839db890c7900418f04144eacc7cf0db0222
tusharay1511 <tusharay1511@gmail.com>
2024-09-27 14:14:02 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,6 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+
+    // Loop through the Roman string
+    for (let i = 0; i < roman.length; i++) {
+        const currentValue = romanMap[roman[i]];
+        const nextValue = romanMap[roman[i + 1]];
+
+        // If the next value is greater, subtract the current value (e.g., IV -> 5 - 1)
+        if (nextValue > currentValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+    }
+
+    return total;
     
 };
 
